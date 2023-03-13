5798cabffe1cf1e7dad00923564bada329398e20
iagosm <iagosousa201486@gmail.com>
2023-03-12 22:57:34 -0300

    adicinando menu e dashboard do controle de estoque

--- menu.php
+++ menu.php
@@ -16,19 +16,62 @@
 
 <body>
     <header>
-        <h2>Bem vindo Nome</h2>
+        <h2>Bem vindo Nome!</h2>
         <p>Selecione uma das opções abaixo</p>
     </header>
     <div class="container-menu">
+        <div class="menu-column">
         <div class="menu-options">
-            <div class="menu-itens">
-                <a href="">
+        <div class="menu-itens">
+                <a href="dashboard.php">
                 <p>Produto</p>
                 <img src="assets/produtos.svg" alt="">
                 </a>
                 
             </div>
+            <div class="menu-itens">
+                <a href="">
+                <p>Fornecedor</p>
+                <img src="assets/fornecedor.svg" alt="">
+                </a>
+                
+            </div>
+            <div class="menu-itens">
+                <a href="">
+                <p>Categoria</p>
+                <img src="assets/categoria.svg" alt="">
+                </a>
+                
+            </div>
         </div>
+        <!-- side right -->
+        <div class="menu-options">
+        <div class="menu-itens">
+                <a href="">
+                <p>Estoque</p>
+                <img src="assets/estoque.svg" alt="">
+                </a>
+                
+            </div>
+            <div class="menu-itens">
+                <a href="">
+                <p>Saídas</p>
+                <img src="assets/transferencia.svg" alt="">
+                </a>
+                
+            </div>
+            <div class="menu-itens">
+                <a href="index.php">
+                <p>Sair</p>
+                <img src="assets/sair.svg" alt="">
+                </a>
+                
+            </div>
+        </div>
+        </div>
+
+        
+        <!-- Outro menu -->
     </div>
 
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script> -->
